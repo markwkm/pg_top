65135d6268052966e99371fa51df3e91dae16e1d
Mark Wong <markwkm@gmail.com>
2007-10-07 09:48:42 -0700

    Added help information on the 'R' command to show user table statistics.

--- help.h
+++ help.h
@@ -16,6 +16,7 @@ L       - show locks held by a process\n\
 M       - sort by memory usage\n\
 N       - sort by pid\n\
 P       - sort by CPU usage\n\
+R       - show user table statistics\n\
 Q       - show current query of a process\n\
 T       - sort by time\n\
 c       - toggle the display of process commands\n\
